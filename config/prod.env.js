7a010114c8c944225d44e8371630e93df95c1c57
yancy777 <1021600964@qq.com>
2018-02-28 14:49:23 +0800

    323
    2321

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,4 @@
 module.exports = {
   NODE_ENV: '"production"'
-  ,API_ROOT: '"http://www.imreliable.com.cn"'
-  // API_ROOT: '"http://www.imreliable.com.cn"'
+
 }
--- config/test.env.js
+++ config/test.env.js
@@ -1,4 +1,4 @@
 module.exports = {
   NODE_ENV: '"testing"',
-  API_ROOT: '"http://www.imreliable.com.cn"'
+  
 }
